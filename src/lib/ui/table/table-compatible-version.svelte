6babc9ca9b06b0b049fbdd969e26f795cb72c861
angelespejo <angelespejo13@gmail.com>
2025-08-27 06:18:56 +0200

    :construction: wip(all): change locales

--- src/lib/ui/table/table-compatible-version.svelte
+++ src/lib/ui/table/table-compatible-version.svelte
@@ -10,7 +10,7 @@
 <TableFull
 	items={{
 		head : [ m['home.tableSoftware'](), m['home.tableFrom']() ],
-		cell : [ [ 'Final Cut Pro', DATA.APP_INFORMATION.finalcut.firstCompatibleVersion ], [ 'Logic Pro', DATA.APP_INFORMATION.logicpro.firstCompatibleVersion ] ],
+		cell : [ [ 'Final Cut Pro', '>=' + DATA.APP_INFORMATION.finalcut.firstCompatibleVersion ], [ 'Logic Pro', '>=' + DATA.APP_INFORMATION.logicpro.firstCompatibleVersion ] ],
 	}}
 />
 
