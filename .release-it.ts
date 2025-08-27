4b52e03ec28f9a2d843bb41182207126bbcaad3e
angelespejo <angelespejo13@gmail.com>
2025-08-28 00:58:14 +0200

    :bug: fix(env): fix version in files

--- .release-it.ts
+++ .release-it.ts
@@ -23,7 +23,7 @@ export default {
 	plugins : { '@release-it/bumper' : { out : [
 		{
 			file : 'src-tauri/tauri.conf.json',
-			path : 'bundle.version',
+			path : 'version',
 		},
 		{
 			file : 'src-tauri/Cargo.toml',
